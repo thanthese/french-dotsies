d3d1f86da85d0b866723ab6517b670e7f48b96a8
Stephen Mann <thanthese@gmail.com>
2012-04-04 20:41:28 -0500

    first sentence done

--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,26 @@
 var dictionary =
-  { "a": "1"
-  , "b": "2"
-  , "c": "3"
+  { null: null
+
+  , 'ailes': 'wings'
+  , 'battements': 'beats'
+  , 'bleue': 'blue'
+  , 'de': 'of'
+  , 'des': 'of'
+  , 'et': 'and'
+  , 'famille': 'family'
+  , 'la': 'the'
+  , 'le': 'the'
+  , 'min': 'min'
+  , 'minute': 'minute'
+  , 'mouche': 'fly'
+  , 'posait': 'lay'
+  , 'pouvant': 'they can'
+  , 'produire': 'produce'
+  , 'rue': 'street'
+  , 's': 's'
+  , 'se': 'itself'
+  , 'septembre': 'september'
+  , 'une': 'a'
+  , 'à': 'at'
+
   }
